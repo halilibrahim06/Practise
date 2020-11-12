c912a00cf356caea1e6ad1b277ef20a9a3595973
Halil  E <halilerd.uk@gmail.com>
2020-11-12 21:11:14 +0000

    new OddOccurrencesInArray task

--- src/test/java/Codility/OddOccurrencesInArray.java
+++ src/test/java/Codility/OddOccurrencesInArray.java
@@ -2,38 +2,26 @@ package Codility;
 
 public class OddOccurrencesInArray {
 
-    public static int solution(int[] A) {
-        int unpaired = 0;
-
-        for (int number : A) {
-            printBinaryFormat(number);
-            System.out.println(" number===>"+number);
-            unpaired ^= number;
-            printBinaryFormat(unpaired);
-            System.out.println(" unpaired===>"+unpaired);
-            System.out.println("<=========>");
-        }
-        return unpaired;
-    }
 
     public static void main(String[] args) {
+
         int[] A = {9,3,9,3,9,7,9};
 
         int res = solution(A);
-        printBinaryFormat(res);
         System.out.println(" res===>"+res);
-
-        //https://codility.com/demo/results/trainingNRQ4RE-4T9/
     }
-    public static void printBinaryFormat(int number){
-        int binary[] = new int[25];
-        int index = 0;
-        while(number > 0){
-            binary[index++] = number%2;
-            number = number/2;
+
+    public static int solution(int[] A) {
+        // sure there is at least one element
+        int result = A[0];
+
+        // bitwise xor wipes out double elements
+        // only remains odd values
+        // loop for remaining
+        for(int i=1; i<A.length; i++) {
+            result ^= A[i];
         }
-        for(int i = index-1;i >= 0;i--){
-            System.out.print(binary[i]);
-        }
+
+        return result;
     }
 }
